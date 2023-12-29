80f98eac9a0e6e1a359a19c4e1b0abdca0bb4c06
rashdanmd <rashdan.developer@gmail.com>
2023-12-29 19:50:19 +0000

    fix export error in store

--- src/app/log-in.tsx
+++ src/app/log-in.tsx
@@ -1,5 +1,38 @@
 "use client";
 import { useState } from "react";
+import { logIn, logOut } from "../redux/features/auth-slice";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/redux/store";
+
+export default function LogIn() {
+  const [username, setUsername] = useState("");
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const onClickLogIn = () => {};
+
+  const onClickToggle = () => {};
+
+  const onClickLogout = () => {};
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => setUsername(e.target.value)} />
+      <br />
+      <button>Log In</button>
+      <br />
+      <button>Log Out</button>
+      <br />
+      <button>Toggle Moderator Status</button>
+    </div>
+  );
+}
+
+/* WITHOUT REDUX
+
+"use client"; 
+import { useState } from "react";
+
 
 export default function LogIn() {
   const [username, setUsername] = useState("");
@@ -22,3 +55,6 @@ export default function LogIn() {
     </div>
   );
 }
+
+
+*/
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -7,7 +7,7 @@ export const store = configureStore({
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 /** 
 reducer: function that takes in an action
 **/
