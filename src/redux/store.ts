a9a7668fe8b5d45586b43b6f5e427065fa39c5a4
rashdanmd <rashdan.developer@gmail.com>
2023-12-29 18:27:34 +0000

    configure store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import authReducer from "./features/auth-slice";
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    authReducer,
+  },
 });
 
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = ReturnType<typeof store.getState>;
 /** 
 reducer: function that takes in an action
 **/
